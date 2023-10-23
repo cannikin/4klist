a726fa7b1919851820282de8900916fedf0d1993
Rob Cameron <cannikin@fastmail.com>
2023-10-23 11:47:04 -0700

    Updates to include Review, Buy US and Buy UK links

--- parser.rb
+++ parser.rb
@@ -1,4 +1,6 @@
+require 'dotenv/load'
 require 'mechanize'
+require 'vacuum'
 
 class Parser
 
@@ -7,7 +9,7 @@ class Parser
   def initialize(source, options = {})
     @source = source
     @rows = []
-    @header = "Title,Reference Disc,Standout Disc,Native 4k,Dolby Vision,HDR10+,Imax Enhanced,60 FPS,Dolby Atmos,DTS:X,Release Date,Studio"
+    @header = "Title,Reference Disc,Standout Disc,Native 4k,Dolby Vision,HDR10+,Imax Enhanced,60 FPS,Dolby Atmos,DTS:X,Release Date,Studio,Review,Buy US,Buy UK"
     @overrides = options[:overrides]
     @skips = options[:skips]
   end
@@ -15,6 +17,7 @@ class Parser
   def convert
     studio = ''
 
+    agent.redirect_ok = true
     agent.get(source) do |page|
       content = page.search('#k2Container .itemFullText').first.children
       content.each do |entry|
@@ -52,6 +55,7 @@ class Parser
     list.search('li').collect do |item|
       title, reference, standout = parse_title(item)
       date = parse_date(item)
+      store_us, store_uk, review = parse_links(item)
       features = parse_features(title, item)
 
       if title
@@ -67,14 +71,17 @@ class Parser
           features[:dolby_atmos],
           features[:dtsx],
           date,
-          studio
+          studio,
+          review ? %Q[=HYPERLINK("#{review}";"Review")] : '',
+          store_us ? %Q[=HYPERLINK("#{store_us}";"#{store_us.split('/')[2]}")] : '',
+          store_uk ? %Q[=HYPERLINK("#{store_uk}";"#{store_uk.split('/')[2]}")] : '',
         ].join(',')
       end
     end.compact
   end
 
   private def parse_title(node)
-    title = node.search('strong').first.content
+    title = node.search('strong').first.content.strip
     reference = false
     standout = false
 
@@ -96,6 +103,8 @@ class Parser
       title.gsub!(/^(.*?)(:| \(|$)/, '\1, The\2')
     end
 
+    puts "Found #{title}"
+
     [title, reference, standout] unless skip_title?(title)
   end
 
@@ -107,6 +116,46 @@ class Parser
     else
       date = '""'
     end
+  end
+
+  private def parse_links(node)
+    store_us, store_uk, review = nil
+
+    node.search('a').each do |link|
+      if link.content.match 'US' and link['href']
+        if link['href'].match 'amzn.to'
+          store_us = parse_amazon_short_link(link)
+        elsif link['href'].match 'amazon.com/exec/obidos'
+          store_us = parse_amazon_long_link(link)
+        else
+          store_us = link['href']
+        end
+      elsif link.content.match 'UK'
+        store_uk = link['href']
+      elsif link.content.match 'REVIEW'
+        review = "https://thedigitalbits.com#{link['href']}"
+      end
+    end
+
+    return store_us, store_uk, review
+  end
+
+  private def parse_amazon_short_link(link)
+    agent.redirect_ok = false
+    page = agent.get(link['href'])
+
+    if redirect = page.header['location']
+      parts = redirect.split('/')[0..5]
+      return parts.join('/') + '?tag=camerontec014-20'
+    else
+      puts "ERROR: No redirect found for #{node.content}"
+      puts "Link: #{link} page.header['location']: #{page.header['location']}"
+      return link['href']
+    end
+  end
+
+  private def parse_amazon_long_link(link)
+    return link['href'].gsub(/thedigitalbits-20/, ENV['AMAZON_ASSOCIATE_TAG'])
   end
 
   private def parse_features(title, item)
@@ -141,4 +190,21 @@ class Parser
     skips.include?(title)
   end
 
+  # Not working yet, need Amazon to get me access to the Product Advertising API
+  private def short_link(product_id)
+    response = amazon_client.get_items(
+      item_ids: [product_id],
+      response_group: 'ItemAttributes',
+    )
+
+    puts response
+
+  end
+
+  private def amazon_client
+    @amazon_client ||= Vacuum.new(marketplace: 'US',
+                     access_key: ENV['AMAZON_KEY'],
+                     secret_key: ENV['AMAZON_SECRET_KEY'],
+                     partner_tag: ENV['AMAZON_ASSOCIATE_TAG'])
+  end
 end
